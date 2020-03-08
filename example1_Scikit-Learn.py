bc51de443e779b5bd3814911b2458e717cca4636
Jose Ponce Hernando <56114434+kirtast@users.noreply.github.com>
2020-03-08 19:31:07 +0100

    First code for Book's code

---
+++ example1_Scikit-Learn.py
@@ -0,0 +1,18 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+# Load the data
+
+# Prepare the data
+
+
+# Visualize the data
+
+# Select a linear model
+
+# Train the model
+
+# Make a prediction for Cryrus
