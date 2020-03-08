f74767343e5949712d3e2decb826493252c6091f
kirtast <jpon.her@gmail.com>
2020-03-08 19:45:40 +0100

    Load data commit

--- example1_Scikit-Learn.py
+++ example1_Scikit-Learn.py
@@ -5,6 +5,8 @@ import pandas as pd
 import sklearn.linear_model
 
 # Load the data
+oecd_bli=pd.read_csv("oecd_bli_2015.csv",thousands=',')
+gdp_per_capita=pd.read_csv("gdp_per_capita.csv",thousands=',',delimeter='\t',encodin='latin1',na_values="n/a")
 
 # Prepare the data
 
