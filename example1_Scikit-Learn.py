d0dd5f58369bf30f729b0ab4393d9eb059034780
kirtast <jpon.her@gmail.com>
2020-03-08 20:18:22 +0100

    La palabra "delimeter" en este de pandas es errónea,cambio a "sep"

--- example1_Scikit-Learn.py
+++ example1_Scikit-Learn.py
@@ -6,7 +6,7 @@ import sklearn.linear_model
 
 # Load the data
 oecd_bli=pd.read_csv("oecd_bli_2015.csv",thousands=',')
-gdp_per_capita=pd.read_csv("gdp_per_capita.csv",thousands=',',delimeter='\t',encodin='latin1',na_values="n/a")
+gdp_per_capita=pd.read_csv("gdp_per_capita.csv",thousands=',',sep='\t',encoding='latin1',na_values="n/a")
 
 # Prepare the data
 # country_stats=prepare_country_stats(oecd_bli,gdp_per_capita)
